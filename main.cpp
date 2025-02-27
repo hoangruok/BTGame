7922947614affa9a0a66ddf337136ad506a93168
hoangruok <vhhsky2k6@gmail.com>
2025-02-27 22:22:11 +0700

    mouse event

--- main.cpp
+++ main.cpp
@@ -22,23 +22,40 @@ int main(int argc, char *argv[])
     Graphics graphics;
     graphics.init();
 
-    SDL_Texture* background = graphics.loadTexture("bikiniBottom.jpg");
-    graphics.prepareScene(background);
+    SDL_Rect rect;
+    rect.x=100;
+    rect.y=100;
+    rect.h=100;
+    rect.w=100;
+    SDL_SetRenderDrawColor(graphics.renderer, 255, 255, 255, 0 );
+    SDL_RenderFillRect(graphics.renderer, &rect);
+    SDL_RenderPresent(graphics.renderer);
 
-    graphics.presentScene();
-    waitUntilKeyPressed();
+    SDL_Event event;
+    int x, y;
 
-    SDL_Texture* spongeBob = graphics.loadTexture("Spongebob.png");
-    graphics.renderTexture(spongeBob, 200, 200);
+    SDL_GetMouseState(&x, &y);
+    cerr << ((x > 100 && y > 100 && x < 200 && y < 200) ? "In\n" : "Out\n");
 
-	graphics.presentScene();
-	waitUntilKeyPressed();
+    SDL_PollEvent(&event);
+    while (true) {
+        SDL_GetMouseState(&x, &y);
+        //cerr << ((x > 100 && y > 100 && x < 200 && y < 200)...
 
-	SDL_DestroyTexture( spongeBob );
-    spongeBob = NULL;
-    SDL_DestroyTexture( background );
-    background = NULL;
+        SDL_PollEvent(&event);
+        switch (event.type) {
+            case SDL_QUIT:
+                 exit(0);
+                 break;
+            case SDL_MOUSEBUTTONDOWN:
+                 cerr << "Down at (" << x << ", " << y << ")\n";
+                 break;
+            case SDL_MOUSEBUTTONUP:
+                 cerr << "Up at (" << x << ", " << y << ")\n";
+                 break;
+        }
+        SDL_Delay(100);
+    }
 
-    graphics.quit();
-    return 0;
+
 }
